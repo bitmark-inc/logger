9cf06daf50ea67439bb805c5e30d142e7c4c1315
Christopher Hall <hsw@bitmark.com>
2016-02-26 10:41:39 +0800

    tidy up
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- logger.go
+++ logger.go
@@ -1,12 +1,7 @@
-// Copyright (c) 2014-2015 Bitmark Inc.
+// Copyright (c) 2014-2016 Bitmark Inc.
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-// A simple logging layer on top of seelog
-//
-// Implements multiple log channels to a single rotated log file.  Log
-// levels are considered a simple hierarchy with each channel having a
-// single limit, below which logs to that channel are skipped.
 package logger
 
 import (
