b6ba2a2ac48d97047ef1bd4b3e71e07c2f818de1
Aaron Chang <xesuschang@gmail.com>
2020-03-24 13:57:45 +0800

    default log message goes to standard out

--- logger_test.go
+++ logger_test.go
@@ -162,10 +162,6 @@ func checkfile(t *testing.T, s string) {
 }
 
 func TestListLevels(t *testing.T) {
-	setup(t)
-	defer logger.Finalise()
-	defer teardown()
-
 	l, err := logger.ListLevels()
 	assert.Nil(t, err, "wrong ListLevels")
 
@@ -185,11 +181,9 @@ func TestListLevels(t *testing.T) {
 }
 
 func TestUpdateTagLogLevel(t *testing.T) {
-	setup(t)
-	defer teardown()
-
 	mainLog := logger.New("main")
 	mainLog.Debug("This should log")
+	mainLog.Info("This should log")
 
 	err := logger.UpdateTagLogLevel("main", "info")
 	assert.Nil(t, err, "wrong UpdateTagLogLevel")
@@ -201,12 +195,10 @@ func TestUpdateTagLogLevel(t *testing.T) {
 	mainLog.Error("This should log")
 	mainLog.Critical("This should log")
 
-	checkfile(t, `2014-08-12 10:44:35 [WARN] LOGGER: ===== Logging system started =====
-2014-08-12 10:44:35 [DEBUG] main: This should log
-2014-08-12 10:44:35 [INFO] main: This should log
-2014-08-12 10:44:35 [WARN] main: This should log
-2014-08-12 10:44:35 [ERROR] main: This should log
-2014-08-12 10:44:35 [CRITICAL] main: This should log
-2014-08-12 10:44:35 [WARN] LOGGER: ===== Logging system stopped =====
-`)
+	// ensure panic won't cause test to fail
+	defer func() {
+		recover()
+	}()
+
+	logger.Panic("this should log")
 }
